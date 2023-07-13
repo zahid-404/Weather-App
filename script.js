fff64cac02cbaab6a6e9aac36ffe3b9dc2553a2a
Zahid <zahidmohammad495@gmai.com>
2023-07-13 09:19:26 +0530

    Complete html part

---
+++ script.js
@@ -0,0 +1,9 @@
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-container");
+const grantAcessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadinScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
+
